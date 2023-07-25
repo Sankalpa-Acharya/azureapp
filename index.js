746836c66e90375a07b6be7457ddff9025bfce2d
Sankalpa-Acharya <sankalpaacharya6609@gmail.com>
2023-07-25 22:47:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const geoip = require('geoip-lite');
+
+app.get('/',(req,res)=>{
+    const geo = geoip.lookup(req.ip);
+    console.log(req.ip)
+    res.status(200).json(geo)
+})
+app.get('/greet',(req,res)=>{
+   res.send('Welcome to my wesbite, I made this website to learn deployment and for no any other purpose')
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('Listening at port 3000')
+})
