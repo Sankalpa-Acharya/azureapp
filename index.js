e5bd11bcafa3516245c5e2132516dee894febb1d
Sankalpa-Acharya <sankalpaacharya6609@gmail.com>
2023-07-26 21:32:28 +0530

    added port

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const app = express()
 const geoip = require('geoip-lite');
-
+const port = process.env.PORT || 3000
 app.get('/',(req,res)=>{
     const geo = geoip.lookup(req.ip);
     console.log(req.ip)
     res.status(200).json(geo)
 })
+
 app.get('/greet',(req,res)=>{
    res.send('Welcome to my wesbite, I made this website to learn deployment and for no any other purpose')
 })
+
 app.get('/sank',(req,res)=>{
    res.send('SaNk is here!!!')
 })
 
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Listening at port 3000')
 })
