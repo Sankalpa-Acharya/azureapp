37d08fa8d399d64272cc7cc04c63b1dfe86ca46c
Sankalpa-Acharya <sankalpaacharya6609@gmail.com>
2023-07-26 22:14:09 +0530

    tets

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const geoip = require('geoip-lite');
 const port = process.env.PORT || 3000
 app.get('/',(req,res)=>{
-    const geo = geoip.lookup('14.139.122.139');
+    const geo = geoip.lookup(req.ip);
     console.log(req.ip)
     res.status(200).json(geo)
 })
