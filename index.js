3313ff3c2639e00d671ec9914bd9eb4e4b2759b5
Sankalpa-Acharya <sankalpaacharya6609@gmail.com>
2023-07-26 22:16:33 +0530

    asd

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const geoip = require('geoip-lite');
 const port = process.env.PORT || 3000
 app.get('/',(req,res)=>{
+    
     const geo = geoip.lookup(req.ip);
-    console.log(req.ip)
     res.status(200).json(geo)
 })
 
